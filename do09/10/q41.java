99bbd511b1bab25b89436d39dfbd3a92d2f63543
WUMBAch <sas.cuznetsov@ya.ru>
2020-10-11 16:30:50 +0500

    q42

---
+++ do09/10/q41.java
@@ -0,0 +1,43 @@
+//Бинарный поиск
+
+
+import java.util.Scanner;
+class q41 {
+    public static void main (String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число, которое необходимо найти в массиве:");
+        int input = in.nextInt();
+        System.out.print("Введите размер массива:");
+        int arrsize = in.nextInt();
+        System.out.println("Заполните массив:");
+        int[] arr = new int[arrsize];
+        for (int i = 0; i<arrsize; i++){
+            arr[i] = in.nextInt();
+        }
+        int itog = 0;
+        int flag = 0;
+        int min = 0;
+        int max = arr.length - 1;
+        while (min <= max){
+            int mid = min + (max - min) / 2;
+            if (input < arr[mid]){
+                max = mid - 1;
+            }
+            else if (input > arr[mid]){
+                min = mid + 1;
+            }
+            else{
+                flag = 1;
+                itog = mid;
+                break;
+            }
+        }
+        if (flag == 0){
+            System.out.print("Такого числа нет в массиве");
+        }
+        else {
+            System.out.print("Индекс искомого числа в массиве: ");
+            System.out.print(itog);
+        }
+    }
+}
--- do09/10/q42.java
+++ do09/10/q42.java
@@ -1,43 +1,24 @@
-//Бинарный поиск
-
-
+//Дана строка из нескольких слов, разделённых пробелами. Отсортировать слова в ней по алфавиту, т.е. из “bca cba cab acb” получить “acb bca cab cba”. Оценить сложность полученного алгоритма по времени и по памяти. Не использовать встроенные методы сортировки.
 import java.util.Scanner;
 class q42 {
     public static void main (String[] args){
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите число, которое необходимо найти в массиве:");
-        int input = in.nextInt();
-        System.out.print("Введите размер массива:");
-        int arrsize = in.nextInt();
-        System.out.println("Заполните массив:");
-        int[] arr = new int[arrsize];
-        for (int i = 0; i<arrsize; i++){
-            arr[i] = in.nextInt();
+        System.out.print("Введите строку:");
+        String str  = in.nextLine();
+        String[] words = str.split(" ");
+        for (int i=0; i<words.length-1; i++)
+            for (int j=i; j>=0; j--)
+                if (words[j].compareTo(words[j+1])>0) {
+
+                    str = words[j];
+                    words[j] = words[j+1];
+                    words[j+1] = str;
+                }
+        System.out.println("Результат сортировки: ");
+        for (String word : words) {
+            System.out.println(word);
         }
-        int itog = 0;
-        int flag = 0;
-        int min = 0;
-        int max = arr.length - 1;
-        while (min <= max){
-            int mid = min + (max - min) / 2;
-            if (input < arr[mid]){
-                max = mid - 1;
-            }
-            else if (input > arr[mid]){
-                min = mid + 1;
-            }
-            else{
-                flag = 1;
-                itog = mid;
-                break;
-            }
-        }
-        if (flag == 0){
-            System.out.print("Такого числа нет в массиве");
-        }
-        else {
-            System.out.print("Индекс искомого числа в массиве: ");
-            System.out.print(itog);
-        }
+
+
     }
 }
